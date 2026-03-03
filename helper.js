a7dd5fb5d98a7c1877e1e7a1c221e1f4f2523795
REDACTED <REDACTED@REDACTED.REDACTED>
2026-03-03 18:28:41 +0100

    :3

--- helper.js
+++ helper.js
@@ -1,32 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0 WITH bison-exception
 // Copyright © 2024 lum1
 import { XMLParser } from "fast-xml-parser";
-
-const microsoft_root =
-  "https://services.mtps.microsoft.com/serviceapi/products";
-
-async function parsePage(url) {
-  const parsingOptions = {
-    ignoreAttributes: false,
-    attributeNamePrefix: "@_",
-    // preserveOrder: true,
-    unpairedTags: ["hr", "br", "link", "meta"],
-    stopNodes: ["*.pre", "*.script"],
-    processEntities: true,
-    isArray: (tagName) => {
-      if (["title", "h2", "codesnippet"].includes(tagName)) return true;
-    },
-    htmlEntities: true,
-  };
-  const parser = new XMLParser(parsingOptions);
-  const res = await fetch(url);
-  const html = res.body.getReader();
-  console.log(html);
-  var root = parser.parse(res);
-}
-
-function install() {
-  parsePage(microsoft_root);
-}
-
-install();
+const product_root = "https://services.mtps.microsoft.com/serviceapi/products";
+data = fetch(product_root);
